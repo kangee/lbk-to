faeb21b8b3627c8fdbb02b1c91c12b8bb72ab250
Markus Fridolfsson <markjs.fridolfsson@gmail.com>
2015-12-14 07:40:08 +0100

    results from games can now be reported

--- client/js/Tournament/round.js
+++ client/js/Tournament/round.js
@@ -9,14 +9,32 @@ Template.Tournament_round.helpers({
 	},
 
 	Games:function(tournamnet, round){
-		console.log(tournamnet);
-		console.log(round);
 		return tournamnet.Rounds[round-1].Games;
 	},
 	Paired:function(){
 		var tournamnet = Router.current().data().Tournament;
 		var roundIndex = Router.current().data().Round-1;
 		return tournamnet.Rounds[roundIndex].paried;
+	},
+	Name: function(id){
+		var players = Router.current().data().Tournament.Players;
+		
+		for (var i = players.length - 1; i >= 0; i--) {
+			if(players[i].Id === id){
+				return players[i].Name
+			}
+		};
 	}
 });
 
+Template.Tournament_round.events({
+
+	"submit form":function(event){
+		event.preventDefault();
+		var tournamentName = Router.current().data().Tournament.Name;
+		var playerOneScore = event.target[0].value;
+		var playerTwoScore = event.target[1].value;
+		Meteor.call("reportResult" , tournamentName, Router.current().data().Round, this.Table , this.PlayerOne , playerOneScore , this.PlayerTwo , playerTwoScore);
+	}
+
+});
--- server/tornamnets.js
+++ server/tornamnets.js
@@ -11,7 +11,7 @@ Meteor.methods({
 		var _player = Player;
 		var id = new Mongo.ObjectID();
 		_player.Id = id._str;
-		_player.Points = 3;
+		_player.Points = 0;
 		Tournaments.update({ Name: tournamentName },{ $push: { Players: _player }})
 	},
 	removePlayer:function(tournamentName,PlayerID){
@@ -25,38 +25,53 @@ Meteor.methods({
 		var tournament = Tournaments.findOne({Name:tournamentName});
 		var _rounds = tournament.Rounds;
 		var _players = tournament.Players;
-		console.log("before sort");
-		_players.sort(playerSort);
-		console.log("after sort");
 
+		_players.sort(playerSort);
 		var games = matchPlayers(_players);
-		console.log("after matching");
-		console.log(games);
 
 		_rounds[round-1].Games = games;
 		_rounds[round-1].paried = true;
 		Tournaments.update({Name:tournamentName},{$set:{Rounds:_rounds, Players:_players}});
-	}
+	},
+	reportResult:function(tournamentName, round , table , playerOne , playerOneScore , playerTwo , playerTwoScore){
+		var tournament = Tournaments.findOne({Name:tournamentName});
+		var _rounds = tournament.Rounds;
+		var _games = _rounds[round-1].Games;
+		var _players = tournament.Players;
 
+		_games[table].Result = playerOneScore + "-" + playerTwoScore;
+
+		for (var i = _players.length - 1; i >= 0; i--) {
+			if (_players[i].Id === playerOne){
+				_players[i].Points = Number(_players[i].Points) + Number(playerOneScore);
+			}
+			if (_players[i].Id === playerTwo){
+				_players[i].Points = Number(_players[i].Points) + Number(playerTwoScore);
+			}
+		};
+
+		Tournaments.update({Name:tournament.Name},{$set: {Rounds: _rounds, Players: _players}})
+	}
 })
 
 
 
 var playerSort = function(player1, player2){
-	console.log("sorting a pair");
 	return player2.Points - player1.Points
 }
 
 var matchPlayers= function(Players){
-	console.log(Players.length);
-	console.log(Players);
 	var games = [];
+	var j = 0;
 	for (var i = 0; i < Players.length; i=i+2) {
-		console.log("paring game: "+i);
-		games[i] = {
+		games[j] = {
 			PlayerOne: Players[i].Id,
-			PlayerTwo: Players[i+1].Id
+			PlayerTwo: Players[i+1].Id,
+			Result: null,
+			Table: j
+
 		};
+		j++;
 	};
 
 	return games;
