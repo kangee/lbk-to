e866be95edb851b71c1e83c6acc4ae0a877cd285
Markus Fridolfsson <markjs.fridolfsson@gmail.com>
2016-04-01 07:54:28 +0200

    added stubs for end result page

---
+++ client/js/Tournament/result.js
@@ -0,0 +1,30 @@
+Template.Tournament_Result.helpers({
+	Total:function(){
+		return [];
+	},
+	BattlePoints: function(){
+		return [];
+	},
+	Impresion:function(){
+		return [];
+
+	},
+	Expresion:function(){
+		return [];
+
+	},
+	Players:function(){
+		return [];
+
+	},
+	isTo:function(){
+		return false;
+	},
+	tournamentDone:function(){
+		return false;
+	},
+	viewResult: function(){
+		return true;
+	}
+
+});
