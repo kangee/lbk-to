64cfcfe87a4d289b87eeb02312b21fb8225370f9
Markus Fridolfsson <markjs.fridolfsson@gmail.com>
2016-04-21 18:16:45 +0200

    added files for adding code for handeling different rolles

---
+++ client/js/collections.js
@@ -0,0 +1,2 @@
+Meteor.subscribe("tournaments");
+Meteor.subscribe("allUsers");
---
+++ server/administrate.js
@@ -0,0 +1,7 @@
+Meteor.publish("allUsers",function(){
+	console.log(this);
+	if(this.userId){
+    	return Meteor.users.find({},{fields:{username:true, profile:true, isAdmin:true }});
+   	} 
+});
+// 
